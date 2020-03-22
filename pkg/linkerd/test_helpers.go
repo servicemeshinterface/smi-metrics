9b0a0ca519c605906b3c800aefa71756f2eaad40
Tarun Pothulapati <tarunpothulapati@outlook.com>
2020-03-22 14:01:28 +0530

    remove extra mock return arg
    Signed-off-by: Tarun Pothulapati <tarunpothulapati@outlook.com>

--- pkg/linkerd/test_helpers.go
+++ pkg/linkerd/test_helpers.go
@@ -109,7 +109,7 @@ func (a *apiTest) MockQuery(
 		"Query",
 		mock.Anything,
 		mock.MatchedBy(a.MatchQueryParam()),
-		mock.Anything).Return(result, nil, nil).Times(num)
+		mock.Anything).Return(result, nil).Times(num)
 
 	return val, interval
 }
